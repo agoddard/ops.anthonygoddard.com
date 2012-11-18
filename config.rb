bdc20901c11d689dd8c67f87f7b2a6ab5bef7236
Anthony Goddard <anthony@anthonygoddard.com>
2012-11-17 20:55:25 -0500

    initial deploy

--- config.rb
+++ config.rb
@@ -2,10 +2,10 @@
 project_type = :stand_alone
 
 # Publishing paths
-http_path = "/"
-http_images_path = "/images"
-http_fonts_path = "/fonts"
-css_dir = "public/stylesheets"
+http_path = "/ops/"
+http_images_path = "/ops/images"
+http_fonts_path = "/ops/fonts"
+css_dir = "public/ops/stylesheets"
 
 # Local development paths
 sass_dir = "sass"
